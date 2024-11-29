78422fa2b87ec6a90586ceba121664d524827d1a
XLunaXX07 <ucfnyz3@ucl.ac.uk>
2024-11-29 17:13:36 +0000

    Add files via upload

---
+++ CASA0014_YueZhu/arduino_secrets.h
@@ -0,0 +1,6 @@
+#define SECRET_SSID "CE-Hub-Student"
+#define SECRET_PASS "casa-ce-gagarin-public-service"
+#define SECRET_MQTTUSER "student"
+#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
+#define SECRET_MQTTSERVER "mqtt.cetools.org"
+#define SECRET_MQTTPORT 1884
