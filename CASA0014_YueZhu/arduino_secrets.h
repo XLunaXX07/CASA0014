bb76ef768f3dc8cd801afa09cfbc1b44b897cc22
XLunaXX07 <ucfnyz3@ucl.ac.uk>
2024-12-20 16:57:10 +0000

    Delete CASA0014_YueZhu

--- CASA0014_YueZhu/arduino_secrets.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_SSID "CE-Hub-Student"
-#define SECRET_PASS ""
-#define SECRET_MQTTUSER "student"
-#define SECRET_MQTTPASS "ce2021-mqtt-forget-whale"
-#define SECRET_MQTTSERVER "mqtt.cetools.org"
-#define SECRET_MQTTPORT 1884
